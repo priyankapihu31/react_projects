357b419ddb82cf4b0493406664df2fbb893c6924
priyankapihu31 <49819798+priyankapihu31@users.noreply.github.com>
2021-09-28 10:24:19 +0530

    app.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import UseEffect from './component/hooks/useEffect';
+// import UseReducer from './component/hooks/useReducer';
+import Todo from './component/todoreact/todo';
+// import Restuarant from './component/Basics/Restuarant';
+// import UseState from './component/hooks/useState'
+
+
+const App = () => {
+  return (
+    <div>
+      <Todo/>
+      {/* <UseReducer/> */}
+      {/* <UseEffect/> */}
+      {/* <UseState/> */}
+    {/* <Restuarant/> */}
+    </div>
+  )
+}
+
+export default App;
